3307f6cd2b35f1a0dd06724dc8868b572a1716f0
martypdx <marty@martypdx.com>
2017-12-04 16:37:49 -0800

    use css local module to "scope" css to components!

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import './Greeting.scss';
+import style from './Greeting.scss';
 
 export default class Greeting extends Component {
   render() {
     const { salutation, name } = this.props;
     return (
-      <div className="greeting">
+      <div className={style.greeting}>
         {salutation} <span>{name}</span>
       </div>
     );
--- src/GreetingForm.js
+++ src/GreetingForm.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import style from './GreetingForm.scss';
 
 export default class GreetingForm extends Component {
   render() {
     const { salutation, name, onChange } = this.props;
     
     return (
-      <div>
-        <input name="salutation" value={salutation} onChange={onChange}/>
-        <input name="name" value={name} onChange={onChange}/>
+      <div className={style.greeting}>
+        <div>
+          <label>
+            Salutation: 
+            <input name="salutation" value={salutation} onChange={onChange}/>
+          </label>
+        </div>
+        <div>
+          <label>
+            Name: 
+            <input name="name" value={name} onChange={onChange}/>
+          </label>
+        </div>
       </div>
     );
   }
